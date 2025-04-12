30deb996aaa547c01528b392e908c4a68435a200
Reza Naqvi <196975975+rezanaqvi8@users.noreply.github.com>
2025-04-12 11:47:24 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TNS Restaurant</title>
+    </head>
+    <body>
+        <header>
+            <h1 style="text-align: center">TNS Restaurant</h1>
+        </header>
+        <?php
+            echo "<p>Welcome to the TNS Restaurant!</p>";
+        ?>
+    </body>
+</html>
