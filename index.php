b6fa352a7cc4fe38aa88b1218cf2284393d3c079
ReNaq654 <196975975+rezanaqvi8@users.noreply.github.com>
2025-04-20 00:46:21 -0400

    added and modified website

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>TNS Restaurant</title>
+        <title>Desi Catering</title>
     </head>
     <body>
         <header>
-            <h1 style="text-align: center">TNS Restaurant</h1>
+            <h1 style="text-align: center">Desi Catering</h1>
         </header>
-        <?php
-            echo "<p>Welcome to the TNS Restaurant!</p>";
-        ?>
+        <h2>About</h2>
+        <p>Desi Catering is a Greenville, SC based catering service that makes tasty Desi foods at competetive prices.</p>
+        <a href="menu.php"><p>Menu</p></a>
     </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Desi Catering Menu</title>
+    </head>
+    <body>
+        <header>
+            <h1 style="text-align: center">Desi Catering Menu</h1>
+        </header>
+        <h2>Menu</h2>
+        <img src="menu image\menu.png" width="300" style="border-radius: 10px;" alt="Desi Catering Menu" align="middle">
+    </body>
+</html>
