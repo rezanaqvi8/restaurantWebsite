be0f62ed6c93ce239734056710ba7eab4938d07e
Reza Naqvi <196975975+rezanaqvi8@users.noreply.github.com>
2025-05-09 17:21:06 -0400

    update site

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Desi Catering</title>
+        <title>Desi Food Catering</title>
+        <link rel="icon" type="image/png" href="logo image/logo.png">
     </head>
     <body>
         <header>
-            <h1 style="text-align: center">Desi Catering</h1>
+            <h1 style="text-align: center">Desi Food Catering</h1>
         </header>
         <h2>About</h2>
-        <p>Desi Catering is a Greenville, SC based catering service that makes tasty Desi foods at competetive prices.</p>
+        <p>Desi Food Catering is a Greenville, SC based catering service that makes tasty Desi foods.</p>
+        <p>We are committed to making the highest quality and tastiest foods that everyone can enjoy.</p>
         <a href="menu.php"><p>Menu</p></a>
     </body>
 </html>
--- menu.php
+++ menu.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Desi Catering Menu</title>
+        <title>Desi Food Catering Menu</title>
+        <link rel="icon" type="image/png" href="logo image/logo.png">
     </head>
     <body>
         <header>
             <h1 style="text-align: center">Desi Catering Menu</h1>
         </header>
         <h2>Menu</h2>
-        <img src="menu image\menu.png" width="300" style="border-radius: 10px;" alt="Desi Catering Menu" align="middle">
+        <img src="menu image\menu.png" style="border-radius: 10px;" alt="Desi Catering Menu" align="middle">
     </body>
 </html>
